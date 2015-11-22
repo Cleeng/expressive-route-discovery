460f272a3acb13e76cdd7e12e4dd9f696911ea26
Mateusz Tymek <mtymek@gmail.com>
2015-11-22 22:52:54 +0100

    Initial commit

---
+++ src/DiscoverAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ExpressiveDiscovery\Action;
+
+use Zend\Diactoros\Response\JsonResponse;
+
+class DiscoverAction
+{
+    /** @var array */
+    private $routes;
+
+    /**
+     * DiscoverAction constructor.
+     * @param array $routes
+     */
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function __invoke()
+    {
+        $routes = [];
+        foreach ($this->routes as $route) {
+            $routes[] = [
+                'path' => $route['path'],
+                'allowed_methods' => implode(',', $route['allowed_methods'])
+            ];
+        }
+        return new JsonResponse($routes, 200, [], JsonResponse::DEFAULT_JSON_FLAGS | JSON_PRETTY_PRINT);
+    }
+}
---
+++ src/DiscoverActionFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ExpressiveDiscovery\Factory;
+
+use ExpressiveDiscovery\Action\DiscoverAction;
+use Interop\Container\ContainerInterface;
+
+class DiscoverActionFactory
+{
+    public function __invoke(ContainerInterface $services)
+    {
+        return new DiscoverAction($services->get('config')['routes']);
+    }
+}
